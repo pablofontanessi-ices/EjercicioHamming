25703c408e5be5d60bd1b4d6178b10ef40ba1599
pablofontanessi-ices <pablo.fontanessi@ices.edu.ar>
2025-04-29 18:06:33 -0300

    correcion contador

--- Hamming_1/Program.cs
+++ Hamming_1/Program.cs
@@ -10,18 +10,21 @@ namespace Hamming_1
     {
         static void Main(string[] args)
         {
-            string ADN1 = "";
-            string ADN2 = "";
+            String ADN1 = "";
+            String ADN2 = "";
             Console.WriteLine("Ingrese primer cadena");
             ADN1 =  Console.ReadLine();
             Console.WriteLine("Ingrese segunda cadena");
             ADN2 = Console.ReadLine();
             int distancia = 0;
+            
+            
+            
             if (ADN1.Length == ADN2.Length && ADN1.Length > 0 && ADN2.Length > 0)
             {
-                for (int i = 0; i <= ADN1.Length; i++)
+                for (int i = 0; i <= ADN1.Length -1; i++)
                 {
-                    if (ADN1.Substring(i) != ADN2.Substring(i))
+                    if (ADN1.ElementAt(i) != ADN2.ElementAt(i))
                     {
                         distancia++;
                     }        
